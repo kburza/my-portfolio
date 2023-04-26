7b5561459e32fb249190b56c0623a62f2cfec745
Kacper Burza <kacperburza@gmail.com>
2023-04-26 14:48:17 -0400

    default theme dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,11 @@ export default function RootLayout({
       */}
       <head />
       <body className="dark:bg-stone-900">
-        <ThemeProvider enableSystem={false} attribute="class">
+        <ThemeProvider
+          defaultTheme="Dark"
+          enableSystem={false}
+          attribute="class"
+        >
           <Navbar />
           {children}
           <Footer />
