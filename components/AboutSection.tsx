b41521174bd208088417792a73595c3ebacbe372
Kacper Burza <65431499+kburza@users.noreply.github.com>
2023-05-21 14:32:11 -0400

    Removed item icon glitch for about section

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -94,7 +94,6 @@ const AboutSection = () => {
                     className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
                   >
                     {item.skill}
-                    {item.icon}
                   </p>
                 );
               })}
