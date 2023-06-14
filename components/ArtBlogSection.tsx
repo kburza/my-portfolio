7a967384fbe41a0dac6c5aeb2d1e5d46d158ec94
Kacper Burza <65431499+kburza@users.noreply.github.com>
2023-06-14 18:36:07 -0400

    Updated artblog
    - Arashi Furniture additions
    - Linktree link property added to art aelements

--- components/ArtBlogSection.tsx
+++ components/ArtBlogSection.tsx
@@ -3,8 +3,24 @@ import Image from "next/image";
 import Link from "next/link";
 import { BsArrowUpRightSquare, BsGithub } from "react-icons/bs";
 import SlideUp from "./SlideUp";
+import { SiLinktree } from "react-icons/si";
 
 const art_elements = [
+  {
+    name: "Arashi Business Card (Front)",
+    description:
+      "Front design of Arashi Art Decor's business card. The marketing material features a color palette that mactehs the website's design as well as a composite promotional image of a wood resin table.",
+    image:
+      "https://demo-bucket-kap.s3.amazonaws.com/artblog/Arashi_Business_Card_Front.jpeg",
+  },
+  {
+    name: "Arashi Business Card (Back)",
+    description:
+      "Front design of Arashi Art Decor's business card. The art features a composite promotional image of a wood resin table (similar to the front of the business card) as well as a scannable QR code which directs the user to the business' Linktree (for main webage, social medias, etc.) ",
+    linktree: "https://linktr.ee/arashiartdecor",
+    image:
+      "https://demo-bucket-kap.s3.amazonaws.com/artblog/Arashi_Business_Card_Back.jpeg",
+  },
   {
     name: "eMiL",
     description:
@@ -55,23 +71,20 @@ const art_elements = [
 ];
 
 const ArtBlogSection = () => {
-  const resumeUrl =
-    "https://www.dropbox.com/s/clwhabhsyt7iof9/Kacper%20Burza%20Resume%202023.pdf?raw=1";
-
   return (
     <section
       id="artblog"
       className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4"
     >
       <div className="flex flex-col  my-10 py-16 space-y-28 w-full max-w-5xl">
-        {art_elements.map((art_elements, idx) => {
+        {art_elements.map((art_element, idx) => {
           return (
             <div key={idx} className="relative">
               <SlideUp offset="-300px 0px -300px 0px">
                 <div className="flex flex-col items-center animate-slideUpCubiBezier animation-delay-2 md:flex-col md:space-x-0">
                   <div className="md:w-1/2 relative z-10 mx-auto">
                     <Image
-                      src={art_elements.image}
+                      src={art_element.image}
                       alt=""
                       width={1000}
                       height={1000}
@@ -81,11 +94,24 @@ const ArtBlogSection = () => {
                   </div>
                   <div className="mt-8 md:w-1/2 mx-auto text-center">
                     <h1 className="text-4xl font-bold mb-6">
-                      {art_elements.name}
+                      {art_element.name}
                     </h1>
                     <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
-                      {art_elements.description}
+                      {art_element.description}
                     </p>
+                    {art_element.linktree && (
+                      <div
+                        className="flex justify-center"
+                        style={{ paddingTop: "4px" }}
+                      >
+                        <Link href={art_element.linktree} target="_blank">
+                          <SiLinktree
+                            size={30}
+                            className="hover:-translate-y-1 transition-transform cursor-pointer"
+                          />
+                        </Link>
+                      </div>
+                    )}
                     <div className="flex flex-row align-bottom space-x-4"></div>
                   </div>
                 </div>
